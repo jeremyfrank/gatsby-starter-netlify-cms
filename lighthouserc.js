a00f673bb7a71b73f2f6a25dee6fd186173909a5
Jeremy Frank <jeremyjfrank@gmail.com>
2021-04-16 15:21:42 -0400

    Change resource size assertions to warnings

--- lighthouserc.js
+++ lighthouserc.js
@@ -10,8 +10,8 @@ module.exports = {
     assert: {
       assertions: {
         'first-contentful-paint': ['error', {'maxNumericValue': 2000}],
-        'resource-summary:document:size': ['error', {'maxNumericValue': 20000}],
-        'resource-summary:image:size': ['error', {'maxNumericValue': 1500000}],
+        'resource-summary:document:size': ['warn', {'maxNumericValue': 20000}],
+        'resource-summary:image:size': ['warn', {'maxNumericValue': 1500000}],
       }
     },
     upload: {
