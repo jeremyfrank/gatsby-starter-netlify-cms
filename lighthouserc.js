c96b1c1c8ddcc030ae626a0d6bcb4e12478fe339
Jeremy Frank <jeremyjfrank@gmail.com>
2021-04-16 15:21:55 -0400

    Update lighthouse config to use core web vitals

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,12 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'first-contentful-paint': ['error', {'maxNumericValue': 2000}],
+        'largest-contentful-paint': ['warn', {'maxNumericValue': 2500}],
+        'largest-contentful-paint': ['error', {'maxNumericValue': 4000}],
+        'total-blocking-time': ['warn', {'maxNumericValue': 300}],
+        'total-blocking-time': ['error', {'maxNumericValue': 600}],
+        'cumulative-layout-shift': ['warn', {'maxNumericValue': 0.1}],
+        'cumulative-layout-shift': ['error', {'maxNumericValue': 0.25}],
         'resource-summary:document:size': ['warn', {'maxNumericValue': 20000}],
         'resource-summary:image:size': ['warn', {'maxNumericValue': 1500000}],
       }
